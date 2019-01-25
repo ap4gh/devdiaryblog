b20870a0b2f23e10d3c7c7476bb56f81a4eed04d
A. Pandey <pandey.amrit@outlook.com>
2019-01-25 18:41:35 +0530

    post-title class and margin on the bottom nav of blog post

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -16,7 +16,7 @@ class BlogPostTemplate extends React.Component {
     return (
       <Layout location={this.props.location} title={siteTitle}>
         <SEO title={post.frontmatter.title} description={post.excerpt} />
-        <h1>{post.frontmatter.title}</h1>
+        <h1 className="post-title">{post.frontmatter.title}</h1>
         <p
           style={{
             ...scale(-1 / 5),
@@ -42,6 +42,7 @@ class BlogPostTemplate extends React.Component {
             justifyContent: `space-between`,
             listStyle: `none`,
             padding: 0,
+            marginTop: `3rem`
           }}
         >
           <li>
