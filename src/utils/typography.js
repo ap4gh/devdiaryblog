44dc4da0350cd61dad01a03b893c3491adff8e16
A. Pandey <pandey.amrit@outlook.com>
2019-06-03 15:53:59 +0530

    typography update

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -11,6 +11,10 @@ Wordpress2016.overrideThemeStyles = () => {
     },
     'h1.post-title': {
       fontSize: `1.9rem`
+    },
+    'h1, h2, h3, h4, h5, h6': {
+      fontFamily: 'Montserrat',
+      fontWeight: 600
     }
   }
 }
